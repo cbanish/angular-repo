70fa43f1e4a97676f8dd4be6b7bd4a0486b7fd13
cbanish <cbanish.balakrishnan@gmail.com>
2019-05-08 10:46:39 +0530

    Reactive anguar form project

--- first-app/src/app/components/user/user.component.ts
+++ first-app/src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Statement } from '@angular/compiler';
 
 @Component({
   selector: 'app-user',
@@ -7,13 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  userName:string;
+  name:string;
   age:number;
+  email:string;
+  address:{
+    street:string,
+    city:string,
+    state:any
+
+  };
+  address1:Address;
+  hobbies:any[];
+
   constructor() { }
 
   ngOnInit() {
-    this.userName='Ramesh';
+    this.name='Ramesh';
     this.age=28;
+    this.email='test@gmail.com';
+    this.address={
+      street:'1st street',
+      city:'NY',
+      state:'CAL'
+    }
+    this.hobbies=['Listenng Music','Watching cricket',1];
+    this.address1={
+      street:'2nd street',
+      city:'NY',
+      state:'CAL'
+    }
   }
+
+}
+
+interface Address{
+  street:string,
+  city:string,
+  state:any
 
 }
