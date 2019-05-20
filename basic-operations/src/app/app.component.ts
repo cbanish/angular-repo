14bb7b146d725802e8096f047313262aff6a05c6
cbanish <cbanish.balakrishnan@gmail.com>
2019-05-20 10:49:11 +0530

    Component Interaction

--- basic-operations/src/app/app.component.ts
+++ basic-operations/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-operations';
+
+  public parentName="App Component";
 }
--- basic-operations/src/app/component/test/test.component.ts
+++ basic-operations/src/app/component/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -17,9 +17,37 @@ export class TestComponent implements OnInit {
   public isSuccess:boolean=false;
   public highlightColor="orange";
 
+  public greeting="";
+
+  public name="";
+
+  public displayCondition=true;
+
+  public color="red";
+
+  public months=['january','feb','mar','apr'];
+
   public messageClasses = {
   "text-success":!this.hasErrors,
   "text-failure":this.hasErrors,
   "text-special":this.isSuccess,
   }
+
+  onClick(event){
+    console.log(event);
+    this.greeting="Welcome to event binding";
+  }
+
+  onClickBind(value){
+    console.log(value);
+  }
+
+ @Input('parentData') paramName :string; 
+
+ @Output() public  childEvent = new EventEmitter();
+
+ fireEvent(){
+ this.childEvent.emit('From Test Component');
+ }
+
 }
